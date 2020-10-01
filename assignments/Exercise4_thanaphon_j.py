335d57215c9e12290ffacde24079565b179e38cf
toytam <toytam@hotmail.com>
2020-10-01 21:02:50 +0700

    Create Exercise4_thanaphon_j.py

---
+++ assignments/Exercise4_thanaphon_j.py
@@ -0,0 +1,14 @@
+
+FE = 50   # Foundation
+GB = 70   # General
+ICS = 75  # Introduction
+CP = 90   # Computer
+
+print(" ----------- Your Score --------------")
+
+print("Foundation English :              ", FE)
+print("General Business :                ", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :            ", CP)
+
+print(" -------------------------------------")
